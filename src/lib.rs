3c75731268512048858db1d212e8f453747a8554
CramBL <marcbkonig@gmail.com>
2023-02-11 20:41:06 +0100

    document unsafe code block

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,9 @@ pub trait ByteSlice {
     fn to_byte_slice(&self) -> &[u8];
 }
 
-/// WARNING: if T is a struct you HAVE TO implement repr(packed) because PADDING is UNITIALIZED MEMORY -> UNDEFINED BEHAVIOR!
+/// # Safety
+/// This function can only be used to serialize a struct if it has the #[repr(packed)] attribute
+/// If there's any padding on T, it is UNITIALIZED MEMORY and therefor UNDEFINED BEHAVIOR!
 pub unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
     // Create read-only reference to T as a byte slice, safe as long as no padding bytes are read
     ::core::slice::from_raw_parts((p as *const T) as *const u8, ::core::mem::size_of::<T>())
