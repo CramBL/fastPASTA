56ea5401132fedc086760381be4f21aafcd48cb5
Marc Konig <marc.beck.konig@cern.ch>
2023-02-14 15:52:20 +0100

    benchmarking of rdh sanity checks

--- benches/io_benchmark.rs
+++ benches/io_benchmark.rs
@@ -138,10 +138,68 @@ fn bench_serialization_write(c: &mut Criterion) {
     group.finish();
 }
 
+// Sanity checking on RDHs
+#[inline]
+fn sanity_check_rdhs(rdh_cru_size_bytes: u64, filename: &str, iterations: usize) {
+    let filepath = std::path::PathBuf::from(filename);
+    let file = file_open_read_only(&filepath).unwrap();
+    let mut buf_reader = std::io::BufReader::new(file);
+    let rdh_validator = fastpasta::validators::rdh::RDH_CRU_V7_VALIDATOR;
+    let mut rdhs = 0;
+
+    loop {
+        let tmp_rdh = match RdhCRUv7::load(&mut buf_reader) {
+            Ok(rdh) => rdh,
+            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
+                print!("EOF reached! ");
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+        };
+        let relative_offset =
+            RelativeOffset::new((tmp_rdh.offset_new_packet as u64) - rdh_cru_size_bytes);
+        buf_reader
+            .seek_relative(relative_offset.0)
+            .expect("Error seeking");
+        match rdh_validator.sanity_check(&tmp_rdh) {
+            Ok(_) => (),
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+        rdhs += 1;
+        if rdhs == iterations {
+            break;
+        }
+    }
+}
+
+fn bench_rdh_sanity_check(c: &mut Criterion) {
+    let mut group = c.benchmark_group("rdh_sanity_check");
+    const RDH_CRU_SIZE_BYTES: u64 = 64;
+    let filename = "../fastpasta_test_files/data_ols_its-ul-v0.5_3.4GB";
+    for i in [1000, 10000, 50000, 100000, 1000000].iter() {
+        group.bench_with_input(BenchmarkId::new("manual", i.to_string()), i, |b, i| {
+            b.iter(|| {
+                sanity_check_rdhs(
+                    black_box(RDH_CRU_SIZE_BYTES),
+                    black_box(filename),
+                    black_box(*i),
+                )
+            })
+        });
+    }
+    group.finish();
+}
+
 criterion_group!(
     benches,
     bench_buffer_capacity,
     bench_deserialization,
-    bench_serialization_write
+    bench_serialization_write,
+    bench_rdh_sanity_check
 );
 criterion_main!(benches);
