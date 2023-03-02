d11111dc76ec0a6961d671d087948a8b62fbe535
Marc Konig <marc.beck.konig@cern.ch>
2023-03-02 17:09:00 +0100

    no longer writes to stdout if checks enabled and no output specified

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use fastpasta::util::bufreader_wrapper::BufferedReaderWrapper;
 use fastpasta::util::config::Opt;
 use fastpasta::util::file_pos_tracker::FilePosTracker;
-use fastpasta::util::file_scanner::FileScanner;
+use fastpasta::util::file_scanner::InputScanner;
 use fastpasta::util::process_v7;
 use fastpasta::util::stats::{self};
 use fastpasta::words::rdh::{Rdh0, RdhCRUv6};
@@ -13,6 +13,7 @@ use log::trace;
 use std::io;
 use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
+use std::thread::JoinHandle;
 use structopt::StructOpt;
 
 fn init_error_logger(cfg: &Opt) {
@@ -23,21 +24,29 @@ fn init_error_logger(cfg: &Opt) {
         .expect("Failed to initialize logger");
 }
 
-pub fn main() {
-    let opt: Opt = StructOpt::from_args();
-    init_error_logger(&opt);
-    trace!("Starting fastpasta with args: {:#?}", opt);
-    if let Err(e) = opt.arg_validate() {
+fn get_config() -> Arc<Opt> {
+    let mut cfg = Opt::from_args();
+
+    if let Err(e) = cfg.arg_validate() {
         eprintln!("{}", e);
         std::process::exit(1);
     }
-    match opt.output_mode() {
+
+    cfg.sort_link_args();
+
+    match cfg.output_mode() {
         fastpasta::util::config::DataOutputMode::Stdout => trace!("Data ouput set to stdout"),
         fastpasta::util::config::DataOutputMode::File => trace!("Data ouput set to file"),
         fastpasta::util::config::DataOutputMode::None => trace!("Data ouput set to suppressed"),
     }
 
-    let config: Arc<Opt> = Arc::new(opt);
+    Arc::new(cfg)
+}
+
+pub fn main() {
+    let config = get_config();
+    init_error_logger(&config.clone());
+    trace!("Starting fastpasta with args: {:#?}", config);
 
     // Launch statistics thread
     // If max allowed errors is reached, stop the processing from the stats thread
@@ -48,7 +57,7 @@ pub fn main() {
     // Determine RDH version
     let rdh0 = Rdh0::load(&mut readable).expect("Failed to read first RDH0");
     let rdh_version = rdh0.header_id;
-    let loader = FileScanner::new_from_rdh0(
+    let loader = InputScanner::new_from_rdh0(
         config.clone(),
         readable,
         FilePosTracker::new(),
@@ -71,7 +80,7 @@ pub fn main() {
 // 3. Write data out (file or stdout)
 pub fn process_rdh_v7(
     config: Arc<Opt>,
-    loader: FileScanner<impl BufferedReaderWrapper + ?Sized + std::marker::Send + 'static>,
+    loader: InputScanner<impl BufferedReaderWrapper + ?Sized + std::marker::Send + 'static>,
     send_stats_ch: std::sync::mpsc::Sender<stats::StatType>,
     thread_stopper: Arc<AtomicBool>,
 ) -> io::Result<()> {
@@ -88,24 +97,29 @@ pub fn process_rdh_v7(
     );
 
     // 3. Write data out
-    let writer_handle = process_v7::output::spawn_writer(
-        config.clone(),
-        thread_stopper.clone(),
-        send_stats_ch,
-        checker_rcv_channel,
-    );
+    let writer_handle: Option<JoinHandle<()>> = match config.output_mode() {
+        fastpasta::util::config::DataOutputMode::None => None,
+        _ => Some(process_v7::output::spawn_writer(
+            config.clone(),
+            thread_stopper.clone(),
+            send_stats_ch,
+            checker_rcv_channel.expect("Checker receiver channel not initialized"),
+        )),
+    };
 
     reader_handle.join().expect("Error joining reader thread");
     validator_handle
         .join()
         .expect("Error joining checker thread");
-    writer_handle.join().expect("Error joining writer thread");
+    if let Some(writer) = writer_handle {
+        writer.join().expect("Error joining writer thread");
+    }
     Ok(())
 }
 
 pub fn process_rdh_v6(
     config: Arc<Opt>,
-    loader: FileScanner<impl BufferedReaderWrapper + ?Sized>,
+    loader: InputScanner<impl BufferedReaderWrapper + ?Sized>,
     send_stats_ch: std::sync::mpsc::Sender<stats::StatType>,
     thread_stopper: Arc<AtomicBool>,
 ) -> io::Result<()> {
