ad83210130e79b2bb203355af87758d254f19f6b
CramBL <marcbkonig@gmail.com>
2023-03-03 18:47:14 +0100

    ignoring test that reads from file without creating it

--- src/words/rdh.rs
+++ src/words/rdh.rs
@@ -607,6 +607,7 @@ mod tests {
 
     // Verifies that the RdhCruv7 struct is serialized and deserialized correctly
     #[test]
+    #[ignore] // Large test ignored in normal cases, useful for debugging
     fn test_load_rdhcruv7() {
         // Create an instace of an RDH-CRU v7
         // write it to a file
@@ -671,6 +672,7 @@ mod tests {
 
     // Verifies that the RdhCruv6 struct is serialized and deserialized correctly
     #[test]
+    #[ignore] // Large test ignored in normal cases, useful for debugging
     fn test_load_rdhcruv6() {
         // Create an instace of an RDH-CRU v6
         // write it to a file
