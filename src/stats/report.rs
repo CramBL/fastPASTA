dda0279598d83cc37b6da56c90b8b3bc3ba1ffcd
Marc Konig <marc.beck.konig@cern.ch>
2023-03-09 10:35:02 +0100

    add fatal error header to report

--- src/stats/report.rs
+++ src/stats/report.rs
@@ -47,6 +47,8 @@ pub struct Report {
     filter_stats_table: Option<Table>,
     detected_attributes: Vec<DetectedAttribute>,
     processing_time: std::time::Duration,
+    fatal_error: Option<String>,
+    report_table: Option<Table>,
 }
 impl Report {
     pub fn new(processing_time: std::time::Duration) -> Self {
@@ -55,6 +57,8 @@ impl Report {
             detected_attributes: Vec::new(),
             processing_time,
             filter_stats_table: None,
+            fatal_error: None,
+            report_table: None,
         }
     }
     pub fn add_filter_stats(&mut self, filter_stats_table: Table) {
@@ -69,7 +73,10 @@ impl Report {
             detected,
         });
     }
-    pub fn print(&self) {
+    pub fn add_fatal_error(&mut self, error: String) {
+        self.fatal_error = Some(error);
+    }
+    pub fn print(&mut self) {
         let mut global_stats_table = Table::new(&self.stats);
         global_stats_table = format_global_stats_sub_table(&global_stats_table);
         let mut detected_attributes_table = Table::new(&self.detected_attributes);
@@ -85,19 +92,31 @@ impl Report {
                 "Filter Stats".to_string(),
                 SubtableColor::Purple,
             );
-
-            let mut multi_table = tabled::col![
+            let multi_table = tabled::col![
                 global_stats_table,
                 tabled::row![detected_attributes_table, filter_stats_table]
             ];
-            multi_table = format_super_table(&multi_table, self.processing_time);
-            eprintln!("{multi_table}");
+            self.report_table = Some(format_super_table(&multi_table, self.processing_time));
         } else {
-            let mut multi_table =
+            let multi_table =
                 tabled::col![global_stats_table, tabled::row![detected_attributes_table]];
-            multi_table = format_super_table(&multi_table, self.processing_time);
-            eprintln!("{multi_table}");
+            self.report_table = Some(format_super_table(&multi_table, self.processing_time));
         }
+        if self.fatal_error.is_some() {
+            let mut error_table = self.report_table.clone().unwrap();
+            error_table
+                .with(Panel::header("FATAL ERROR - EARLY TERMINATION"))
+                .with(
+                    Modify::new(Rows::single(0))
+                        .with(Alignment::center())
+                        .with(Format::new(|x| {
+                            let x = x.to_uppercase();
+                            x.red().to_string()
+                        })),
+                );
+            self.report_table = Some(error_table);
+        }
+        eprintln!("{}", self.report_table.as_ref().unwrap());
     }
 }
 
@@ -227,44 +246,46 @@ mod tests {
         object::{Columns, Rows},
     };
 
-    #[test]
-    fn test_print_filtered_links() {
-        let sum = StatSummary::new(
-            "Filtered links".to_string(),
-            String::from("0"),
-            Some(String::from("Not found: 2")),
-        );
-        let sum2 = StatSummary::new(
-            "Links observed".to_string(),
-            "1 32 4 5 6 7 8 9".to_string(),
-            None,
-        );
-        let sum_vec = vec![sum, sum2];
-        let mut table = Table::new(sum_vec);
+    // macro_rules! assert_stdout_eq {
+    //     ($test:expr, $expected:literal) => {{
+    //         use gag::BufferRedirect;
+    //         use std::io::Read;
 
-        table
-            .with(Panel::header("Report"))
-            .with(Panel::footer("3 elements"));
+    //         let mut buf = BufferRedirect::stdout().unwrap();
 
-        table
-            //.with(style)
-            .with(Modify::new(Rows::single(1)).with(Format::new(|x| x.to_uppercase())))
-            .with(Modify::new(Columns::single(0)).with(Format::new(|s| s.blue().to_string())))
-            .with(
-                Modify::new(Columns::single(1)).with(Format::new(|s| s.bright_cyan().to_string())),
-            )
-            .with(Modify::new(Columns::new(2..)).with(Format::new(|s| s.yellow().to_string())));
+    //         $test;
+
+    //         let mut output = String::new();
+    //         buf.read_to_string(&mut output).unwrap();
+    //         drop(buf);
+
+    //         assert_eq!(&output[..], $expected);
+    //     }};
+    // }
+
+    macro_rules! assert_stderr_contains {
+        ($test:expr, $expected:literal) => {{
+            use gag::BufferRedirect;
+            use std::io::Read;
+
+            let mut buf = BufferRedirect::stderr().unwrap();
+
+            $test;
+
+            let mut output = String::new();
+            buf.read_to_string(&mut output).unwrap();
+            drop(buf);
+
+            assert!(output.contains($expected));
+        }};
     }
 
     #[test]
-    fn test_summary() {
+    fn test_summary_contains_filtered_links_rdhs() {
         let processing_time = std::time::Instant::now();
         let mut report = Report::new(processing_time.elapsed());
-        report.add_stat(StatSummary::new(
-            "Total errors".to_string(),
-            "0".to_string(),
-            None,
-        ));
+        let statistic_tot_errs = "Total errors".to_string();
+        report.add_stat(StatSummary::new(statistic_tot_errs, "0".to_string(), None));
         report.add_stat(StatSummary::new(
             "Total RDHs".to_string(),
             "725800".to_string(),
@@ -281,6 +302,26 @@ mod tests {
         let filter_table = Table::new(vec![filtered_links, observed_links]);
         report.add_filter_stats(filter_table);
 
-        report.print();
+        assert_stderr_contains!(report.print(), "Total errors");
+        assert_stderr_contains!(report.print(), "Total RDHs");
+        assert_stderr_contains!(report.print(), "725800");
+    }
+
+    #[test]
+    fn test_fatal_error_report() {
+        let processing_time = std::time::Instant::now();
+        let fatal_error = "Fatal Error happened";
+        let mut report = Report::new(processing_time.elapsed());
+        report.add_fatal_error(fatal_error.to_string());
+
+        assert_stderr_contains!(report.print(), "FATAL ERROR");
+    }
+
+    #[test]
+    fn test_fatal_error_report_panic() {
+        let processing_time = std::time::Instant::now();
+        let fatal_error = "Fatal Error happened";
+        let mut report = Report::new(processing_time.elapsed());
+        report.add_fatal_error(fatal_error.to_string());
     }
 }
