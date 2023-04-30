8a292c3a9b2dba8b30f414280fd5feac35626d75
CramBL <marcbkonig@gmail.com>
2023-04-30 12:59:51 +0200

    tests for all cmds on 10_rdh.raw

--- tests/fastpasta_10_rdh.rs
+++ tests/fastpasta_10_rdh.rs
@@ -6,6 +6,22 @@ mod fastpasta;
 use crate::fastpasta::match_on_output;
 use crate::fastpasta::FILE_10_RDH; // File used in these tests
 
+// Asserts that the end of processing report summary contains correct information
+fn validate_report_summary(byte_output: &Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+    let match_patterns = vec![
+        "(?i)Trigger Type.*0x6A03",
+        "(?i)Trigger Type.*SOC",
+        "(?i)RDH.*Version.*7",
+        "(?i)Total.*RDHs.*10",
+        "(?i)Total.*hbfs.*5",
+        "(?i)((layers)|(staves)).*((layers)|(staves)).*L0_12",
+    ];
+    match_patterns.into_iter().for_each(|pattern| {
+        assert!(match_on_output(byte_output, pattern, 1));
+    });
+    Ok(())
+}
+
 #[test]
 fn view_rdh() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastpasta")?;
@@ -55,21 +71,77 @@ fn check_sanity() -> Result<(), Box<dyn std::error::Error>> {
         "(?i)warn - ",
         0
     ));
-    // Asserts on stdout
-    let stdout = &cmd.output().unwrap().stdout;
-    let match_patterns = vec![
-        "(?i)Trigger Type.*0x6A03",
-        "(?i)Trigger Type.*SOC",
-        "(?i)RDH.*Version.*7",
-        "(?i)Total.*RDHs.*10",
-        "(?i)Total.*hbfs.*5",
-        "(?i)((layers)|(staves)).*((layers)|(staves)).*L0_12",
-    ];
-    let expected_matches = 1;
 
-    match_patterns.into_iter().for_each(|pattern| {
-        assert!(match_on_output(stdout, pattern, expected_matches));
-    });
+    validate_report_summary(&cmd.output().unwrap().stdout)?;
+
+    Ok(())
+}
+
+#[test]
+fn check_sanity_its() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_10_RDH).arg("check").arg("sanity").arg("its");
+    cmd.assert().success();
+
+    assert!(match_on_output(
+        &cmd.output().unwrap().stderr,
+        "(?i)error - ",
+        0
+    ));
+    assert!(match_on_output(
+        &cmd.output().unwrap().stderr,
+        "(?i)warn - ",
+        0
+    ));
+    // Asserts on stdout
+    validate_report_summary(&cmd.output().unwrap().stdout)?;
+
+    Ok(())
+}
+
+#[test]
+fn check_all() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_10_RDH).arg("check").arg("all");
+    cmd.assert().success();
+
+    assert!(match_on_output(
+        &cmd.output().unwrap().stderr,
+        "(?i)error - ",
+        0
+    ));
+    assert!(match_on_output(
+        &cmd.output().unwrap().stderr,
+        "(?i)warn - ",
+        0
+    ));
+    // Asserts on stdout
+    validate_report_summary(&cmd.output().unwrap().stdout)?;
+
+    Ok(())
+}
+
+#[test]
+fn check_all_its() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_10_RDH).arg("check").arg("all").arg("its");
+    cmd.assert().success();
+
+    assert!(match_on_output(
+        &cmd.output().unwrap().stderr,
+        "(?i)error - ",
+        0
+    ));
+    assert!(match_on_output(
+        &cmd.output().unwrap().stderr,
+        "(?i)warn - ",
+        0
+    ));
+    // Asserts on stdout
+    validate_report_summary(&cmd.output().unwrap().stdout)?;
 
     Ok(())
 }
