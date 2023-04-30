17081a161bc446dcd4a9eff0bb27c5866519f159
CramBL <marcbkonig@gmail.com>
2023-04-30 11:27:44 +0200

    Add more fancy tests with fancy_regex

--- tests/fastpasta.rs
+++ tests/fastpasta.rs
@@ -2,6 +2,13 @@ use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
+/// Path to test files : tests/regression/test-data/
+/// Files
+const FILE_10_RDH: &str = "tests/regression/test-data/10_rdh.raw";
+const FILE_ERR_NOT_HBF: &str = "tests/regression/test-data/err_not_hbf.raw";
+const FILE_THRS_CDW_LINKS: &str = "tests/regression/test-data/thrs_cdw_links.raw";
+const FILE_READOUT_SUPERPAGE_1: &str = "tests/regression/test-data/readout.superpage.1.raw";
+
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastpasta")?;
@@ -18,10 +25,7 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 fn file_exists_exit_successful() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastpasta")?;
 
-    cmd.arg("tests/regression/test-data/10_rdh.raw")
-        .arg("check")
-        .arg("sanity")
-        .arg("-v2");
+    cmd.arg(FILE_10_RDH).arg("check").arg("sanity").arg("-v2");
     cmd.assert().success();
 
     Ok(())
@@ -31,10 +35,7 @@ fn file_exists_exit_successful() -> Result<(), Box<dyn std::error::Error>> {
 fn view_rdh() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastpasta")?;
 
-    cmd.arg("tests/regression/test-data/10_rdh.raw")
-        .arg("view")
-        .arg("rdh")
-        .arg("-v2");
+    cmd.arg(FILE_10_RDH).arg("view").arg("rdh").arg("-v2");
 
     use predicate::str::is_match;
     cmd.assert()
@@ -48,9 +49,7 @@ fn view_rdh() -> Result<(), Box<dyn std::error::Error>> {
 fn view_hbf() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastpasta")?;
 
-    cmd.arg("tests/regression/test-data/readout.superpage.1.raw")
-        .arg("view")
-        .arg("hbf");
+    cmd.arg(FILE_READOUT_SUPERPAGE_1).arg("view").arg("hbf");
 
     cmd.assert().success().stdout(
         predicate::str::contains("IHW").count(3).and(
@@ -61,6 +60,31 @@ fn view_hbf() -> Result<(), Box<dyn std::error::Error>> {
             ),
         ),
     );
+
+    Ok(())
+}
+
+#[test]
+fn err_not_hbf_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+    let re = fancy_regex::Regex::new(r"(?i)ERrOR - 0xa0.*pages").unwrap(); // case insensitive
+    let pred_one_match = predicate::function(|&x| re.find_iter(x).count() == 1);
+
+    let test_is_false = pred_one_match.eval(&"test");
+    assert!(!test_is_false);
+    let test_is_true = pred_one_match.eval(&"error - 0xa0 something pages something");
+    assert!(test_is_true);
+
+    cmd.arg(FILE_ERR_NOT_HBF).arg("check").arg("all");
+
+    cmd.assert().success();
+
+    // Take the output of stderr and convert it to string
+    let res = cmd.output().unwrap().stderr;
+    let str_res = std::str::from_utf8(&res).expect("invalid utf-8 sequence");
+
+    // Compare with regex predicate
+    assert!(pred_one_match.eval(&str_res));
 
     Ok(())
 }
