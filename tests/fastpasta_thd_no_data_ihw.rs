a580ddb60308ab44a0c17c0949659bcbb7e34666
CramBL <marcbkonig@gmail.com>
2023-05-05 10:32:13 +0200

    add test

--- tests/fastpasta_thd_no_data_ihw.rs
+++ tests/fastpasta_thd_no_data_ihw.rs
@@ -1,3 +1,5 @@
+use predicates::str::contains;
+
 use crate::util::*;
 mod util;
 
@@ -65,6 +67,28 @@ fn check_all_its_issue_29() -> Result<(), Box<dyn std::error::Error>> {
     assert!(match_on_output(&cmd.output()?.stderr, "(?i)warn - ", 0));
 
     validate_report_summary(&cmd.output()?.stdout)?;
+
+    Ok(())
+}
+
+// https://gitlab.cern.ch/mkonig/fastpasta/-/issues/29
+#[test]
+fn check_view_hbf_issue_29() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_TDH_NO_DATA_IHW).arg("view").arg("hbf");
+    cmd.assert().success();
+
+    assert!(match_on_output(&cmd.output()?.stderr, "(?i)error - ", 0));
+    assert!(match_on_output(&cmd.output()?.stderr, "(?i)warn - ", 0));
+
+    // Before issue 29 was fixed, 0x5320 was erroneously displayed as a TDH.
+    // Here we confirm that it is now correctly interpreted as an IHW
+    cmd.assert().stdout(
+        contains("5320: IHW")
+            .count(1)
+            .and(contains("5320: IHW [FF 3F 00 00 00 00 00 00 00 E0]")),
+    );
 
     Ok(())
 }
