c0722bcb61d5e9028a916e3630544569028eea90
CramBL <marcbkonig@gmail.com>
2023-05-06 17:01:40 +0200

    fix arg validation

--- src/util/lib.rs
+++ src/util/lib.rs
@@ -11,13 +11,13 @@ pub trait Config: Util + Filter + InputOutput + Checks + Views + Send + Sync {
     fn validate_args(&self) -> Result<(), String> {
         if let Some(check) = self.check() {
             if let Some(target) = check.target() {
-                if matches!(target, super::config::System::ITS_Stave)
-                    && self.filter_its_stave().is_none()
-                {
-                    return Err("Cannot check ITS stave without specifying a stave".to_string());
+                if matches!(target, super::config::System::ITS_Stave) {
+                    if self.filter_its_stave().is_none() {
+                        return Err("Cannot check ITS stave without specifying a stave".to_string());
+                    }
+                } else if self.check_its_trigger_period().is_some() {
+                    return Err("Specifying trigger period has to be done with the `check all its_stave` command".to_string());
                 }
-            } else if self.check_its_trigger_period().is_some() {
-                return Err("Specifying trigger priod has to be done with the `check all its_stave` command".to_string());
             }
         }
         Ok(())
