24ce97b86c060764e721620f65a0eaa2f52f1911
CramBL <marcbkonig@gmail.com>
2023-05-06 19:35:34 +0200

    add view tests on 1_hbf_bad_tdt.raw

--- tests/fastpasta_1_hbf_bad_tdt.rs
+++ tests/fastpasta_1_hbf_bad_tdt.rs
@@ -56,3 +56,38 @@ fn check_all_its_stave() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn view_rdh() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_1_HBF_BAD_TDT)
+        .arg("view")
+        .arg("rdh")
+        .arg("-v4");
+    cmd.assert().success();
+
+    assert_no_errors_or_warn(&cmd.output().unwrap().stderr)?;
+    // match lines that have the RDH version 7, header size 64, and feeid 524
+    match_on_out_no_case(&cmd.output().unwrap().stdout, ".*7.*64.*524", 2)?;
+
+    Ok(())
+}
+
+#[test]
+fn view_its_readout_frames() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_1_HBF_BAD_TDT)
+        .arg("view")
+        .arg("its-readout-frames")
+        .arg("-v4");
+    cmd.assert().success();
+
+    // There is an error on a ITS status word (the TDT) so we expect to see an error
+    match_on_out_no_case(&cmd.output().unwrap().stderr, "error.*id.*0xF1", 1)?;
+    // match lines that have the RDH version 7, header size 64, and feeid 524
+    match_on_out_no_case(&cmd.output().unwrap().stdout, ": RDH", 2)?;
+
+    Ok(())
+}
