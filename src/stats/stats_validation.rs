dbfe7ab461db0431bfe18c0ed1fe26f91b8e2518
CramBL <marcbkonig@gmail.com>
2023-07-05 14:08:43 +0200

    add custom check for physics trigger count

--- src/stats/stats_validation.rs
+++ src/stats/stats_validation.rs
@@ -10,14 +10,20 @@ pub fn validate_custom_stats(
     if let Some(cdps) = custom_checks.cdps() {
         if rdh_stats.rdhs_seen() != cdps as u64 {
             errors.push(format!(
-                "[E99] Expected {expected_cdps} CDPS, but found {observed_cdps}",
+                "[E99] Expected {expected_cdps} CDPs, but found {observed_cdps}",
                 expected_cdps = cdps,
                 observed_cdps = rdh_stats.rdhs_seen()
             ));
         }
     }
-    if let Some(triggers_sent) = custom_checks.triggers_pht() {
-        todo!();
+    if let Some(expect_triggers_pht) = custom_checks.triggers_pht() {
+        if rdh_stats.trigger_stats().pht() != expect_triggers_pht {
+            errors.push(format!(
+                "[E99] Expected {expected_triggers_pht} PhT triggers, but found {observed_triggers_pht}",
+                expected_triggers_pht = expect_triggers_pht,
+                observed_triggers_pht = rdh_stats.trigger_stats().pht()
+            ));
+        }
     }
 
     if errors.is_empty() {
