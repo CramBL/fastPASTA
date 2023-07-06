ed01bf8bba20d0e917931305323a25f417ea6cf7
CramBL <marcbkonig@gmail.com>
2023-07-06 16:05:16 +0200

    test chip ID order custom check and no custom check

--- tests/fastpasta_rawtf_epn180_l6_1.rs
+++ tests/fastpasta_rawtf_epn180_l6_1.rs
@@ -220,3 +220,70 @@ fn check_all_its_stave_mute_errors() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn check_all_its_stave_no_chip_order_errors() -> Result<(), Box<dyn std::error::Error>> {
+    // Checks that the chip order errors are not present now that they are optional (custom check)
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    const EXPECTED_ALPIDE_ERRORS: u8 = 18; // Bunch counter mismatch errors
+
+    cmd.arg(FILE_RAWTF_EPN180_L6_1)
+        .arg("check")
+        .arg("all")
+        .arg("its-stave");
+    cmd.assert().success();
+
+    match_on_out_no_case(
+        &cmd.output()?.stdout,
+        &format!("errors.*{EXPECTED_ALPIDE_ERRORS}"),
+        1,
+    )?;
+
+    match_on_out_no_case(&cmd.output()?.stderr, "chip id order", 0)?;
+
+    Ok(())
+}
+
+#[test]
+fn check_all_its_stave_custom_chip_id_order_errors() -> Result<(), Box<dyn std::error::Error>> {
+    // Check that errors are present when a custom check on the chip id order is used
+    let custom_checks_str = r#"
+# Number of CRU Data Packets expected in the data
+# Example value: 20 [type: u32]
+# cdps = 10
+# Number of Physics (PhT) Triggers expected in the data
+# Example value: 20 [type: u32]
+triggers_pht = 0 # PhT triggers are only expected in triggered mode
+chip_orders_ob = [[0, 1, 2, 3, 4, 5, 6], [8, 9, 10, 11, 12, 13, 14]]
+"#;
+    let custom_checks_file_name = "check_all_its_stave_custom_chip_id_order_errors.toml";
+    let tmp_dir = assert_fs::TempDir::new()?;
+    let tmp_custom_checks_path = tmp_dir.path().join(custom_checks_file_name);
+    let mut custom_checks_file = std::fs::File::create(tmp_custom_checks_path.clone())?;
+    std::io::Write::write_all(&mut custom_checks_file, custom_checks_str.as_bytes())?;
+
+    custom_checks_file.sync_all()?;
+
+    const EXPECTED_ALPIDE_ERRORS: u8 = 18;
+
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+    cmd.arg(FILE_RAWTF_EPN180_L6_1)
+        .arg("check")
+        .arg("all")
+        .arg("its-stave")
+        .arg("--checks-toml")
+        .arg(tmp_custom_checks_path);
+
+    cmd.assert().success();
+
+    match_on_out_no_case(
+        &cmd.output()?.stdout,
+        &format!("errors.*{EXPECTED_ALPIDE_ERRORS}"),
+        1,
+    )?;
+
+    match_on_out_no_case(&cmd.output()?.stderr, "chip id order", 18)?;
+
+    Ok(())
+}
