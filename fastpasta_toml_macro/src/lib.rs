f4d93dc743e7a0824ea720a8548c05e334a240a8
CramBL <marcbkonig@gmail.com>
2023-07-07 09:16:37 +0200

    Better comments

--- fastpasta_toml_macro/src/lib.rs
+++ fastpasta_toml_macro/src/lib.rs
@@ -1,3 +1,7 @@
+/// This trait is derived through the [TomlConfig](fastpasta_toml_macro_derive::TomlConfig) derive macro.
 pub trait TomlConfig {
+    /// Generates a customized pretty [String] representation of the serialized struct as a `TOML` template.
+    /// The template includes comments with all possible fields and their types, that is easily edited and deserializes into the struct it was serialized from.
+    /// The template also includes comments with descriptions, and examples.
     fn to_string_pretty_toml(&self) -> String;
 }
