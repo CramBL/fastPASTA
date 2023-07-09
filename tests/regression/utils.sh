9b6e7ae6ea2af2b2bc43b7322e19d302f3750d26
CramBL <marcbkonig@gmail.com>
2023-07-09 12:29:34 +0200

    clean up

--- tests/regression/utils.sh
+++ tests/regression/utils.sh
@@ -3,46 +3,46 @@
 
 # Printing utility
 
-TXT_CLEAR="\e[0m"
+# TXT_CLEAR="\e[0m" (reverts to default color)
 
-TXT_YELLOW="\e[33m"
+# TXT_YELLOW="\e[33m"
 function println_yellow {
-    printf "${TXT_YELLOW}${1}${TXT_CLEAR}\n"
+    printf  "\e[33m%b\e[0m\n" "${1}"
 }
 
-TXT_BRIGHT_CYAN="\e[96m"
+#TXT_BRIGHT_CYAN="\e[96m"
 function println_cyan {
-    printf "${TXT_BRIGHT_CYAN}${1}${TXT_CLEAR}\n"
+    printf "\e[96m%b\e[0m\n" "${1}"
 }
 
-TXT_RED="\e[31m"
+#TXT_RED="\e[31m"
 function println_red {
-    printf "${TXT_RED}${1}${TXT_CLEAR}\n"
+    printf "\e[31m%b\e[0m\n" "${1}"
 }
 
-TXT_GREEN="\e[32m"
+# TXT_GREEN="\e[32m"
 function println_green {
-    printf "${TXT_GREEN}${1}${TXT_CLEAR}\n"
+    printf "\e[32m%b\e[0m\n" "${1}"
 }
 
-TXT_BRIGHT_GREEN="\e[92m"
+# TXT_BRIGHT_GREEN="\e[92m"
 function println_bright_green {
-    printf "${TXT_BRIGHT_GREEN}${1}${TXT_CLEAR}\n"
+    printf "\e[92m%b\e[0m\n" "${1}"
 }
 
-TXT_BLUE="\e[34m"
+# TXT_BLUE="\e[34m"
 function println_blue {
-    printf "${TXT_BLUE}${1}${TXT_CLEAR}\n"
+    printf "\e[34m%b\e[0m\n" "${1}"
 }
 
-TXT_BRIGHT_MAGENTA="\e[95m"
+# TXT_BRIGHT_MAGENTA="\e[95m"
 function println_magenta {
-    printf "${TXT_BRIGHT_MAGENTA}${1}${TXT_CLEAR}\n"
+    printf "\e[95m%b\e[0m\n" "${1}"
 }
 
-TXT_BRIGHT_YELLOW="\e[93m"
+# TXT_BRIGHT_YELLOW="\e[93m"
 function println_bright_yellow {
-    printf "${TXT_BRIGHT_YELLOW}${1}${TXT_CLEAR}\n"
+    printf "\e[93m%b\e[0m\n" "${1}"
 }
 
 # WARNING: Does not correctly compare between two negative floats. Not relevant for the use case though...
@@ -95,18 +95,21 @@ function float_cmp() {
 
 function evaluate_benchmark_test_result {
     local_mean_timing=${1}; remote_mean_timing=${2}
+
     println_yellow "\n\tLocal fastpasta timing : ${local_mean_timing} ms (mean)"
     println_yellow "\n\tRemote fastpasta timing: ${remote_mean_timing} ms (mean)"
 
-    local_minus_remote=$((${local_mean_timing}-${remote_mean_timing}))
-    remote_minus_local=$((${remote_mean_timing}-${local_mean_timing}))
+    local_minus_remote=$(( local_mean_timing - remote_mean_timing ))
+    remote_minus_local=$(( remote_mean_timing - local_mean_timing ))
 
     println_bright_yellow "\t\tdifference between local and remote build: ${local_minus_remote} ms"
 
     if [[ "${local_mean_timing}" -lt ${remote_mean_timing} ]]; then
         println_green "\t\t\t-> local build is faster by ${remote_minus_local} ms!"
+
     elif [[ "${local_mean_timing}" -gt ${remote_mean_timing} ]]; then
         println_red "\t\t\t-> local build is slower by ${local_minus_remote} ms..."
+
     elif [[ "${local_mean_timing}" -eq ${remote_mean_timing} ]]; then
         println_blue "\t\t\t-> local and remote are about equally fast"
     fi
