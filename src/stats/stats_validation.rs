b5d9330351dbc9bd842f4f22f466994b36b60f5e
CramBL <marcbkonig@gmail.com>
2023-07-13 16:19:01 +0200

    unique error codes

--- src/stats/stats_validation.rs
+++ src/stats/stats_validation.rs
@@ -10,7 +10,7 @@ pub fn validate_custom_stats(
     if let Some(cdps) = custom_checks.cdps() {
         if rdh_stats.rdhs_seen() != cdps as u64 {
             errors.push(format!(
-                "[E99] Expected {expected_cdps} CDPs, but found {observed_cdps}",
+                "[E9001] Expected {expected_cdps} CDPs, but found {observed_cdps}",
                 expected_cdps = cdps,
                 observed_cdps = rdh_stats.rdhs_seen()
             ));
@@ -19,7 +19,7 @@ pub fn validate_custom_stats(
     if let Some(expect_triggers_pht) = custom_checks.triggers_pht() {
         if rdh_stats.trigger_stats().pht() != expect_triggers_pht {
             errors.push(format!(
-                "[E99] Expected {expected_triggers_pht} PhT triggers, but found {observed_triggers_pht}.\n{triggers_stats}",
+                "[E9002] Expected {expected_triggers_pht} PhT triggers, but found {observed_triggers_pht}.\n{triggers_stats}",
                 expected_triggers_pht = expect_triggers_pht,
                 observed_triggers_pht = rdh_stats.trigger_stats().pht(),
                 triggers_stats = rdh_stats.trigger_stats()
