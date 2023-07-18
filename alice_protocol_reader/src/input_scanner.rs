763b16c3f6f12c51d833dcc318be854748e2ff4b
CramBL <marcbkonig@gmail.com>
2023-07-18 19:56:49 +0200

    better way to collect initial stats

--- alice_protocol_reader/src/input_scanner.rs
+++ alice_protocol_reader/src/input_scanner.rs
@@ -149,17 +149,20 @@ where
     #[inline(always)]
     fn load_rdh_cru<T: RDH>(&mut self) -> Result<T, std::io::Error> {
         // If it is the first time we get an RDH, we would already have loaded the initial RDH0
-        //  from the input. If so, we use it to create the first RDH, and record some stats
-        //  that should only be recorded once.
+        //  from the input. If so, we use it to create the first RDH.
         let rdh: T = if self.initial_rdh0.is_some() {
             let rdh: T =
                 SerdeRdh::load_from_rdh0(&mut self.reader, self.initial_rdh0.take().unwrap())?;
-            // Report general initial stats assumed to be the same for the rest of the data
-            self.initial_collect_stats(&rdh)?;
+
             rdh
         } else {
             SerdeRdh::load(&mut self.reader)?
         };
+
+        if self.current_mem_pos() == 0 {
+            // Report general initial stats assumed to be the same for the rest of the data
+            self.initial_collect_stats(&rdh)?;
+        }
 
         // Collect stats
         self.collect_rdh_seen_stats(&rdh);
