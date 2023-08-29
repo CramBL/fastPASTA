2b00f94e1b3856a04dfa9ac5ba2cf032490793fa
Marc Beck Konig <marc.beck.konig@cern.ch>
2023-08-29 16:54:32 +0200

    add data from lab ci with chip disabled

--- fastpasta/tests/fastpasta.rs
+++ fastpasta/tests/fastpasta.rs
@@ -255,6 +255,22 @@ fn file_exists_exit_successful_invalid_lane_order_1rdh() -> Result<(), Box<dyn s
     Ok(())
 }
 
+#[test]
+fn file_exists_exit_successful_ci_ols_data_1hbf() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_CI_OLS_DATA_1HBF)
+        .arg("check")
+        .arg("sanity")
+        .arg("-v4");
+    cmd.assert().success();
+
+    // Take the output of stderr and match it with a pattern once
+    match_on_out_no_case(&cmd.output().unwrap().stderr, "exit success", 1)?;
+
+    Ok(())
+}
+
 /// Check that a not found file returns a fatal error, with a description of an OS error
 ///
 /// Try with all the different verbosity values 0-4
