2c2a8c17c87967f6319946b9ca43b472dec0c2df
CramBL <marcbkonig@gmail.com>
2023-09-20 14:51:55 +0200

    rename to stats_collector to stats

--- fastpasta/src/stats/stats_report.rs
+++ fastpasta/src/stats/stats_report.rs
@@ -23,76 +23,71 @@ use report::Report;
 /// Helper function that makes the report
 pub fn make_report(
     processing_time: std::time::Duration,
-    stats_collector: &mut StatsCollector,
+    stats: &mut StatsCollector,
     filter_target: Option<FilterTarget>,
 ) -> Report {
-    stats_collector.finalize();
+    stats.finalize();
 
     let mut report = Report::new(processing_time);
 
-    if stats_collector.fatal_err() {
-        report.add_fatal_error(stats_collector.take_fatal_err().into_string());
+    if stats.fatal_err() {
+        report.add_fatal_error(stats.take_fatal_err().into_string());
     }
 
     // Add global stats
-    add_global_stats_to_report(&mut report, stats_collector);
+    add_global_stats_to_report(&mut report, stats);
 
     if filter_target.is_some() {
-        let filtered_stats: Vec<StatSummary> = add_filtered_stats(stats_collector, filter_target);
+        let filtered_stats: Vec<StatSummary> = add_filtered_stats(stats, filter_target);
         report.add_filter_stats(tabled::Table::new(filtered_stats));
     } else {
         // Check if the observed system ID is ITS
-        if matches!(stats_collector.system_id(), Some(SystemId::ITS)) {
+        if matches!(stats.system_id(), Some(SystemId::ITS)) {
             // If no filtering, the layers and staves seen is from the total RDHs
             report.add_stat(summerize_layers_staves_seen(
-                stats_collector.layer_staves_as_slice(),
-                stats_collector.staves_with_errors_as_slice(),
+                stats.layer_staves_as_slice(),
+                stats.staves_with_errors_as_slice(),
             ));
         }
         // If no filtering, the HBFs seen is from the total RDHs
         report.add_stat(StatSummary::new(
             "Total HBFs".to_string(),
-            stats_collector.hbfs_seen().to_string(),
+            stats.hbfs_seen().to_string(),
             None,
         ));
 
         // If no filtering, the payload size seen is from the total RDHs
-        report.add_stat(summerize_data_size(
-            stats_collector.rdhs_seen(),
-            stats_collector.payload_size(),
-        ));
+        report.add_stat(summerize_data_size(stats.rdhs_seen(), stats.payload_size()));
     }
 
     // Add ALPIDE stats (if they are collected)
-    if let Some(alpide_stats) = stats_collector.take_alpide_stats() {
+    if let Some(alpide_stats) = stats.take_alpide_stats() {
         add_alpide_stats_to_report(&mut report, alpide_stats);
     }
 
     // Add detected attributes
-    add_detected_attributes_to_report(&mut report, stats_collector.rdh_stats());
+    add_detected_attributes_to_report(&mut report, stats.rdh_stats());
 
     report
 }
 
 /// Helper function that adds the global stats to the report
-fn add_global_stats_to_report(report: &mut Report, stats_collector: &mut StatsCollector) {
-    if stats_collector.err_count() == 0 {
+fn add_global_stats_to_report(report: &mut Report, stats: &mut StatsCollector) {
+    if stats.err_count() == 0 {
         report.add_stat(StatSummary::new(
             "Total Errors".green().to_string(),
-            stats_collector.err_count().green().to_string(),
+            stats.err_count().green().to_string(),
             None,
         ));
     } else {
         report.add_stat(StatSummary::new(
             "Total Errors".red().to_string(),
-            stats_collector.err_count().red().to_string(),
-            Some(format_error_codes(
-                stats_collector.unique_error_codes_as_slice(),
-            )),
+            stats.err_count().red().to_string(),
+            Some(format_error_codes(stats.unique_error_codes_as_slice())),
         ));
     }
 
-    let (trigger_type_raw, trigger_type_str) = stats_collector.rdh_stats().run_trigger_type();
+    let (trigger_type_raw, trigger_type_str) = stats.rdh_stats().run_trigger_type();
     report.add_stat(StatSummary {
         statistic: "Run Trigger Type".to_string(),
         value: format!("{trigger_type_raw:#02X}"),
@@ -100,55 +95,55 @@ fn add_global_stats_to_report(report: &mut Report, stats_collector: &mut StatsCo
     });
     report.add_stat(StatSummary::new(
         "Total RDHs".to_string(),
-        stats_collector.rdh_stats().rdhs_seen().to_string(),
+        stats.rdh_stats().rdhs_seen().to_string(),
         None,
     ));
     report.add_stat(StatSummary::new(
         "Links observed".to_string(),
-        format_links_observed(stats_collector.rdh_stats().links_as_slice()),
+        format_links_observed(stats.rdh_stats().links_as_slice()),
         None,
     ));
     report.add_stat(StatSummary::new(
         "FEE IDs seen".to_string(),
-        format_fee_ids(stats_collector.rdh_stats().fee_ids_as_slice()),
+        format_fee_ids(stats.rdh_stats().fee_ids_as_slice()),
         None,
     ));
 }
 
 /// Helper function that builds a vector of the stats associated with the filtered data
 fn add_filtered_stats(
-    stats_collector: &StatsCollector,
+    stats: &StatsCollector,
     filter_target: Option<FilterTarget>,
 ) -> Vec<StatSummary> {
     let mut filtered_stats: Vec<StatSummary> = Vec::new();
     filtered_stats.push(StatSummary::new(
         "RDHs".to_string(),
-        stats_collector.rdh_stats().rdhs_filtered().to_string(),
+        stats.rdh_stats().rdhs_filtered().to_string(),
         None,
     ));
     // If filtering, the HBFs seen is from the filtered RDHs
     filtered_stats.push(StatSummary::new(
         "HBFs".to_string(),
-        stats_collector.rdh_stats().hbfs_seen().to_string(),
+        stats.rdh_stats().hbfs_seen().to_string(),
         None,
     ));
 
     filtered_stats.push(summerize_data_size(
-        stats_collector.rdh_stats().rdhs_filtered(),
-        stats_collector.rdh_stats().payload_size(),
+        stats.rdh_stats().rdhs_filtered(),
+        stats.rdh_stats().payload_size(),
     ));
 
     if let Some(filter_target) = filter_target {
         let filtered_target = match filter_target {
             FilterTarget::Link(link_id) => {
-                summerize_filtered_links(link_id, stats_collector.rdh_stats().links_as_slice())
+                summerize_filtered_links(link_id, stats.rdh_stats().links_as_slice())
             }
             FilterTarget::Fee(fee_id) => {
-                summerize_filtered_fee_ids(fee_id, stats_collector.rdh_stats().fee_ids_as_slice())
+                summerize_filtered_fee_ids(fee_id, stats.rdh_stats().fee_ids_as_slice())
             }
             FilterTarget::ItsLayerStave(fee_id_no_link) => summerize_filtered_its_layer_staves(
                 fee_id_no_link,
-                stats_collector.rdh_stats().layer_staves_as_slice(),
+                stats.rdh_stats().layer_staves_as_slice(),
             ),
         };
         filtered_stats.push(filtered_target);
@@ -156,11 +151,11 @@ fn add_filtered_stats(
 
     if filter_target.is_some_and(|target| !matches!(target, FilterTarget::ItsLayerStave(_))) {
         // Check if the observed system ID is ITS
-        if matches!(stats_collector.rdh_stats().system_id(), Some(SystemId::ITS)) {
+        if matches!(stats.rdh_stats().system_id(), Some(SystemId::ITS)) {
             // If no filtering, the layers and staves seen is from the total RDHs
             filtered_stats.push(summerize_layers_staves_seen(
-                stats_collector.rdh_stats().layer_staves_as_slice(),
-                stats_collector.staves_with_errors_as_slice(),
+                stats.rdh_stats().layer_staves_as_slice(),
+                stats.staves_with_errors_as_slice(),
             ));
         }
     }
