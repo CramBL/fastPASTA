6dbfd31b69e3a9d5612f792632ccbff4f5a3e8a7
CramBL <marcbkonig@gmail.com>
2023-09-20 10:21:15 +0200

    add json serde and test

--- fastpasta/src/stats/trigger_stats.rs
+++ fastpasta/src/stats/trigger_stats.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
+use serde::{Deserialize, Serialize};
 use std::fmt::Display;
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, PartialEq, Clone, Default, Deserialize, Serialize)]
 pub struct TriggerStats {
     orbit: u32,
     // [1] Heart Beat flag
@@ -215,5 +216,87 @@ impl Display for TriggerStats {
         writeln!(f, "  TPC_rst:  {:>6}", self.tpc_rst)?;
         writeln!(f, "  TOF:      {:>6}", self.tof)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_serde_consistency() {
+        // Test JSON and TOML serialization and deserialization
+        let mut trigger_stats = TriggerStats::default();
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0000_0001);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0000_0010);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0000_0100);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0000_1000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0001_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0010_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_0100_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0000_1000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0001_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0010_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_0100_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0000_1000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0001_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0010_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_0100_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0000_1000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0001_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0010_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_0100_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0000_1000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0001_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0010_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_0100_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0000_1000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0001_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0010_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_0100_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0000_1000_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0001_0000_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0010_0000_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b0100_0000_0000_0000_0000_0000_0000_0000);
+        trigger_stats.collect_stats(0b1000_0000_0000_0000_0000_0000_0000_0000);
+
+        // JSON
+        let trigger_stats_ser_json = serde_json::to_string(&trigger_stats).unwrap();
+        let trigger_stats_de_json: TriggerStats =
+            serde_json::from_str(&trigger_stats_ser_json).unwrap();
+        assert_eq!(trigger_stats, trigger_stats_de_json);
+
+        // TOML
+        let trigger_stats_ser_toml = toml::to_string(&trigger_stats).unwrap();
+        let trigger_stats_de_toml: TriggerStats = toml::from_str(&trigger_stats_ser_toml).unwrap();
+        assert_eq!(trigger_stats, trigger_stats_de_toml);
+
+        // Display
+        println!("{}", trigger_stats_ser_json);
+        println!("{}", trigger_stats_ser_toml);
+
+        // Make sure the counters are correct
+        assert_eq!(trigger_stats.orbit(), 1);
+        assert_eq!(trigger_stats.hb(), 1);
+        assert_eq!(trigger_stats.hbr(), 1);
+        assert_eq!(trigger_stats.hc(), 1);
+        assert_eq!(trigger_stats.pht(), 1);
+        assert_eq!(trigger_stats.pp(), 1);
+        assert_eq!(trigger_stats.cal(), 1);
+        assert_eq!(trigger_stats.sot(), 1);
+        assert_eq!(trigger_stats.eot(), 1);
+        assert_eq!(trigger_stats.soc(), 1);
+        assert_eq!(trigger_stats.eoc(), 1);
+        assert_eq!(trigger_stats.tf(), 1);
+        assert_eq!(trigger_stats.fe_rst(), 1);
+        assert_eq!(trigger_stats.rt(), 1);
+        assert_eq!(trigger_stats.rs(), 1);
+        assert_eq!(trigger_stats.lhc_gap1(), 1);
+        assert_eq!(trigger_stats.lhc_gap2(), 1);
+        assert_eq!(trigger_stats.tpc_sync(), 1);
+        assert_eq!(trigger_stats.tpc_rst(), 1);
+        assert_eq!(trigger_stats.tof(), 1);
     }
 }
