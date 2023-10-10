1142d742a4e74f067788f72cb13ed9ab75253956
Marc Beck Konig <marc.beck.konig@cern.ch>
2023-10-10 12:32:19 +0200

    move report error to utility

--- fastpasta/src/analyze/validators/its/util.rs
+++ fastpasta/src/analyze/validators/its/util.rs
@@ -1,4 +1,36 @@
-use crate::words::its::status_words::{Cdw, Ddw0, Ihw, Tdh, Tdt};
+use crate::{
+    stats::StatType,
+    words::its::status_words::{Cdw, Ddw0, Ihw, Tdh, Tdt},
+};
+
+/// Helper function to format and report an error in ITS protocol
+///
+/// Takes in the error string slice and the word slice
+/// Adds the memory position to the error string
+/// Sends the error to the stats channel
+#[inline]
+pub(super) fn report_error(
+    mem_pos: u64,
+    err: &str,
+    word_slice: &[u8],
+    sender: &flume::Sender<StatType>,
+) {
+    sender
+            .send(StatType::Error(format!(
+                "{mem_pos:#X}: {err} [{:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X}]",
+                word_slice[0],
+                word_slice[1],
+                word_slice[2],
+                word_slice[3],
+                word_slice[4],
+                word_slice[5],
+                word_slice[6],
+                word_slice[7],
+                word_slice[8],
+                word_slice[9],
+                            ).into()))
+            .expect("Failed to send error to stats channel");
+}
 
 #[derive(Default)]
 struct TdhBuffer {
