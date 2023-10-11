3178916496d0d7e5415edc19a5bbc47d597ee4ad
Marc Beck Konig <marc.beck.konig@cern.ch>
2023-10-11 15:08:01 +0200

    add tests for error code filtering

--- fastpasta/tests/fastpasta_1_hbf_bad_its_payload.rs
+++ fastpasta/tests/fastpasta_1_hbf_bad_its_payload.rs
@@ -41,6 +41,59 @@ fn check_all_its() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+/// Test that the error code filter specifying both the error codes that are reported in this data prints both the expected errors
+fn check_all_its_with_error_code_filter_allowing_all_errors(
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_1_HBF_BAD_ITS_PAYLOAD)
+        .arg("check")
+        .arg("all")
+        .arg("its")
+        .arg("-v4")
+        .arg("--show-only-errors-with-codes")
+        .arg("40")
+        .arg("72");
+    cmd.assert().success();
+
+    match_on_out_no_case(&cmd.output().unwrap().stderr, "error.*0x50:.*id is not", 1)?;
+    match_on_out_no_case(
+        &cmd.output().unwrap().stderr,
+        "error.*0x70:.*lane 8.*IHW", // Error with lane 8 not being active according to IHW
+        1,
+    )?;
+    match_on_out_no_case(&cmd.output().unwrap().stdout, "total.*errors.*2", 1)?;
+
+    Ok(())
+}
+
+#[test]
+/// Test that the error code filter specifying only one of the error codes that are reported in this data prints only the expected error
+fn check_all_its_with_error_code_filter_one_error() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastpasta")?;
+
+    cmd.arg(FILE_1_HBF_BAD_ITS_PAYLOAD)
+        .arg("check")
+        .arg("all")
+        .arg("its")
+        .arg("-v4")
+        .arg("--show-only-errors-with-codes")
+        .arg("72");
+    cmd.assert().success();
+    // Expect 0 matches because this error is filtered out when only showing error code 72
+    match_on_out_no_case(&cmd.output().unwrap().stderr, "error.*0x50:.*id is not", 0)?;
+    // This has error code 72 so it should still be shown
+    match_on_out_no_case(
+        &cmd.output().unwrap().stderr,
+        "error.*0x70:.*lane 8.*IHW", // Error with lane 8 not being active according to IHW
+        1,
+    )?;
+    match_on_out_no_case(&cmd.output().unwrap().stdout, "total.*errors.*2", 1)?;
+
+    Ok(())
+}
+
+#[test]
 fn check_all_its_stave() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastpasta")?;
 
