926d1b9290c021cc158a195a6385d3808c6340c4
Marc Beck Konig <marc.beck.konig@cern.ch>
2023-10-19 14:48:21 +0200

    allow specifying file path instead of target system in check mode

--- fastpasta/src/config/check.rs
+++ fastpasta/src/config/check.rs
@@ -1,5 +1,7 @@
 //! Trait for all check options.
-use clap::{Args, Subcommand, ValueEnum};
+use std::path::PathBuf;
+
+use clap::{Args, Subcommand};
 /// A config that implements this trait can be used to enable checks.
 pub trait ChecksOpt {
     /// Type of Check to perform.
@@ -45,29 +47,38 @@ where
 }
 
 /// Check subcommand to enable checks, needs to be followed by a check type subcommand and a target system
-#[derive(Subcommand, Debug, Clone, Copy, PartialEq)]
+#[derive(Subcommand, Debug, Clone, PartialEq)]
 pub enum CheckCommands {
     /// Perform sanity & running checks on RDH. If a target system is specified (e.g. 'ITS') checks implemented for the target is also performed. If no target system is specified, only the most generic checks are done.
-    All {
-        /// Optional target system for checks
-        system: Option<System>,
-    },
+    All(CheckModeArgs),
     /// Perform only sanity checks on RDH. If a target system is specified (e.g. 'ITS') checks implemented for the target is also performed. If no target system is specified, only the most generic checks are done.
-    Sanity {
-        /// Optional target system for checks
-        system: Option<System>,
-    },
+    Sanity(CheckModeArgs),
 }
 
-/// Target system for checks
-#[derive(Args, Debug, Clone, Copy, PartialEq)]
-pub struct Target {
-    /// Target system for checks
-    pub system: Option<System>,
+#[derive(Debug, Args, Clone, PartialEq, Default)]
+#[command(args_conflicts_with_subcommands = true)]
+/// Arguments to All/Sanity check modes, default values are [None].
+pub struct CheckModeArgs {
+    #[command(subcommand)]
+    /// A target system to enable checks for
+    pub target: Option<System>,
+
+    /// Placeholder allowing not specifying a system and instead just supplying the path to raw data following the check mode argument
+    ///
+    /// e.g. `check all <rawdata_file.raw>`
+    #[command(flatten)]
+    pub path: CmdPathArg,
+}
+
+#[derive(Debug, Default, Args, Clone, PartialEq)]
+/// The placeholder value for supplying a path to raw data in the position where a target system would otherwise be specified
+pub struct CmdPathArg {
+    #[arg(short = 'r', long)]
+    path: Option<PathBuf>,
 }
 
 /// List of supported systems to target for checks
-#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Subcommand, Copy, Clone, Debug, PartialEq, Eq)]
 pub enum System {
     /// Specify ITS as the target system for checks.
     ITS,
