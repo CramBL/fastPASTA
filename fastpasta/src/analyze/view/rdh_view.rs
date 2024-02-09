aa4b5fb720cdec671e28f7a1aef1b5d84199c19a
Marc Beck König <mbkj@tutamail.com>
2024-02-09 23:40:21 +0100

    reduce padding to reduce wasted space

--- fastpasta/src/analyze/view/rdh_view.rs
+++ fastpasta/src/analyze/view/rdh_view.rs
@@ -12,19 +12,19 @@ pub(crate) fn rdh_view<T: RDH, const CAP: usize>(
     let mut stdio_lock = std::io::stdout().lock();
 
     if Cfg::global().disable_styled_views() {
-        let header_text = RdhCru::rdh_header_text_with_indent_to_string(16);
+        let header_text = RdhCru::rdh_header_text_with_indent_to_string(11);
         writeln!(stdio_lock, "{header_text}")?;
         for (rdh, _, mem_pos) in cdp_array {
-            writeln!(stdio_lock, "{mem_pos:>8X}:       {rdh}")?;
+            writeln!(stdio_lock, "{mem_pos:>8X}:  {rdh}")?;
         }
     } else {
-        let header_text = RdhCru::rdh_header_styled_text_with_indent_to_string(16);
+        let header_text = RdhCru::rdh_header_styled_text_with_indent_to_string(10);
         writeln!(stdio_lock, "{header_text}")?;
         for (rdh, _, mem_pos) in cdp_array {
             writeln!(
                 stdio_lock,
-                "{memory_position}       {styled_rdh}",
-                memory_position = format_args!("{mem_pos:>8X}:").bg_rgb::<51, 0, 51>().bold(),
+                "{memory_position}{styled_rdh}",
+                memory_position = format_args!("{mem_pos:>8X}: ").bg_rgb::<51, 0, 51>().bold(),
                 styled_rdh = rdh.to_styled_row_view()
             )?;
         }
