91f147d2a5b043f9967f0a3bfcc49dd2f0f159f8
Marc Beck König <mbkj@tutamail.com>
2024-02-10 19:46:33 +0100

    colorize its-readout-frame views

--- fastpasta/src/analyze/view/its_readout_frame.rs
+++ fastpasta/src/analyze/view/its_readout_frame.rs
@@ -1,4 +1,7 @@
-use crate::words::its::Stave;
+use crate::{
+    analyze::{validators::its::lib::ItsPayloadWord, view::lib::format_word_slice},
+    words::its::Stave,
+};
 use alice_protocol_reader::prelude::RDH;
 use owo_colors::OwoColorize;
 use std::io::Write;
@@ -6,13 +9,27 @@ use std::io::Write;
 pub mod its_readout_frame_data_view;
 pub mod its_readout_frame_view;
 
-fn mem_pos_calc_to_string(idx: usize, data_format: u8, rdh_mem_pos: u64) -> String {
+fn mem_pos_calc_to_string(
+    idx: usize,
+    data_format: u8,
+    rdh_mem_pos: u64,
+    disable_styled_view: bool,
+) -> String {
     let current_mem_pos = super::lib::calc_current_word_mem_pos(idx, data_format, rdh_mem_pos);
-    format!("{current_mem_pos:>8X}:")
+    if disable_styled_view {
+        format!("{current_mem_pos:>8X}:",)
+    } else {
+        format!(
+            "{}",
+            format_args!("{current_mem_pos:>8X}:",)
+                .white()
+                .bg_rgb::<MEM_POS_RED, 0, 0>()
+        )
+    }
 }
 
 pub const RDH_RED: u8 = 50;
-pub const MEM_POS_RED: u8 = 80;
+pub const MEM_POS_RED: u8 = 10;
 pub const TRIG_TYPE_BLUE: u8 = 30;
 pub const WORD_TYPE_GREEN: u8 = 20;
 pub const PACKET_STATUS_YELLOW_R: u8 = 40;
@@ -107,5 +124,178 @@ fn print_rdh_its_readout_frame_view<T: RDH>(
         )?;
     }
 
+    Ok(())
+}
+
+pub fn generate_status_word_view(
+    word: &[u8],
+    mem_pos_str: &str,
+    stdio_lock: &mut std::io::StdoutLock,
+    disable_styled_view: bool,
+    display_data_words: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    match ItsPayloadWord::from_id(word[9]) {
+        Ok(word_type) => generate_its_readout_frame_word_view(
+            word_type,
+            word,
+            mem_pos_str,
+            stdio_lock,
+            disable_styled_view,
+            display_data_words,
+        )?,
+        Err(e) => {
+            let word_str = format_word_slice(word);
+            let trimmed_mem_pos_str = mem_pos_str.trim();
+            log::error!(
+                "{trimmed_mem_pos_str} {e}: {:#02X} found in: {word_str}",
+                word[9]
+            );
+        }
+    }
+
+    Ok(())
+}
+
+const DATA_WORD_BLUE: u8 = 30;
+const TDH_GREEN: u8 = 60;
+const TDT_GREEN: u8 = 20;
+const IHW_BROWN_R: u8 = 128;
+const IHW_BROWN_G: u8 = 64;
+const DDW_BLUE: u8 = 90;
+const CDW_PURPLE_R: u8 = 64;
+const CDW_PURPLE_B: u8 = 77;
+
+/// Generates a human readable view of ITS readout frame words based on the raw word, word type, and memory position.
+///
+/// Takes:
+///     * The word byte slice
+///     * The type of PayloadWord from the ITS payload protocol
+///     * The memory position of the word
+fn generate_its_readout_frame_word_view(
+    word_type: crate::analyze::validators::its::lib::ItsPayloadWord,
+    gbt_word_slice: &[u8],
+    mem_pos_str: &str,
+    stdio_lock: &mut std::io::StdoutLock,
+    disable_styled_view: bool,
+    display_data_words: bool,
+) -> Result<(), std::io::Error> {
+    use crate::words::its::status_words::util as sw_util;
+
+    let word_slice_str = crate::analyze::view::lib::format_word_slice(gbt_word_slice);
+    match word_type {
+        // Ignore data words
+        ItsPayloadWord::DataWord => {
+            if display_data_words {
+                if disable_styled_view {
+                    writeln!(stdio_lock, "{mem_pos_str} DATA {word_slice_str}")?;
+                } else {
+                    writeln!(
+                        stdio_lock,
+                        "{mem_pos_str} {}",
+                        format_args!("DATA {word_slice_str}")
+                            .white()
+                            .bg_rgb::<0, 0, DATA_WORD_BLUE>()
+                    )?;
+                }
+            }
+        }
+        ItsPayloadWord::TDH => {
+            let trigger_str = sw_util::tdh_trigger_as_string(gbt_word_slice);
+            let continuation_str = sw_util::tdh_continuation_as_string(gbt_word_slice);
+            let no_data_str = sw_util::tdh_no_data_as_string(gbt_word_slice);
+            let trig_orbit_bc_str = sw_util::tdh_trigger_orbit_bc_as_string(gbt_word_slice);
+            let tdh_info_row = format!("TDH {word_slice_str} {trigger_str}  {continuation_str}        {no_data_str} {trig_orbit_bc_str:>42}");
+
+            if disable_styled_view {
+                writeln!(stdio_lock, "{mem_pos_str} {tdh_info_row}")?;
+            } else {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} {}",
+                    tdh_info_row.white().bg_rgb::<0, TDH_GREEN, 0>()
+                )?;
+            }
+        }
+
+        ItsPayloadWord::TDT => {
+            let packet_status_str = sw_util::tdt_packet_done_as_string(gbt_word_slice);
+            let error_reporting_str = sw_util::ddw0_tdt_lane_status_as_string(gbt_word_slice);
+            if disable_styled_view {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} TDT {word_slice_str} {packet_status_str:>18}                             {error_reporting_str}",
+                )?;
+            } else {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} {}",
+                    format_args!("TDT {word_slice_str} {packet_status_str:>18}                             {error_reporting_str}                   ")
+                        .white()
+                        .bg_rgb::<0, TDT_GREEN, 0>()
+                )?;
+            }
+        }
+        ItsPayloadWord::IHW => {
+            if disable_styled_view {
+                writeln!(stdio_lock, "{mem_pos_str} IHW {word_slice_str}")?;
+            } else {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} {}",
+                    format_args!(
+                        "IHW {word_slice_str}                                                                          "
+                    )
+                    .white()
+                    .bg_rgb::<IHW_BROWN_R, IHW_BROWN_G, 0>()
+                )?;
+            }
+        }
+
+        ItsPayloadWord::DDW0 => {
+            let error_reporting_str = sw_util::ddw0_tdt_lane_status_as_string(gbt_word_slice);
+
+            if disable_styled_view {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} DDW {word_slice_str}                                                {error_reporting_str}",
+                )?;
+            } else {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} {}",
+                    format_args!("DDW {word_slice_str}                                                {error_reporting_str}                   ")
+                        .white()
+                        .bg_rgb::<0, 0, DDW_BLUE>()
+                )?;
+            }
+        }
+        ItsPayloadWord::CDW => {
+            if disable_styled_view {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} CDW {word_slice_str}                                                ",
+                )?;
+            } else {
+                writeln!(
+                    stdio_lock,
+                    "{mem_pos_str} {}",
+                    format_args!(
+                        "CDW {word_slice_str}                                                                          "
+                    )
+                    .white()
+                    .bg_rgb::<CDW_PURPLE_R, 0, CDW_PURPLE_B>()
+                )?;
+            }
+        }
+        ItsPayloadWord::IHW_continuation
+        | ItsPayloadWord::TDH_continuation
+        | ItsPayloadWord::TDH_after_packet_done => {
+            unsafe {
+                // This function receives only simple types,
+                //  as they are coming from ItsPayloadWord::from_id() and not from the FSM that can determine more complex types
+                std::hint::unreachable_unchecked()
+            }
+        }
+    }
     Ok(())
 }
--- fastpasta/src/analyze/view/its_readout_frame/its_readout_frame_data_view.rs
+++ fastpasta/src/analyze/view/its_readout_frame/its_readout_frame_data_view.rs
@@ -1,11 +1,6 @@
-use crate::analyze::{
-    validators::{its::lib::ItsPayloadWord, lib::preprocess_payload},
-    view::lib::format_word_slice,
-};
+use crate::analyze::validators::lib::preprocess_payload;
 use alice_protocol_reader::cdp_wrapper::cdp_array::CdpArray;
 use alice_protocol_reader::prelude::*;
-
-use std::io::Write;
 
 pub(crate) fn its_readout_frame_data_view<T: RDH, const CAP: usize>(
     cdp_array: &CdpArray<T, CAP>,
@@ -23,97 +18,19 @@ pub(crate) fn its_readout_frame_data_view<T: RDH, const CAP: usize>(
         let gbt_word_chunks = preprocess_payload(payload)?;
         for (idx, gbt_word) in gbt_word_chunks.enumerate() {
             let word = &gbt_word[..10];
-            let mem_pos_str = super::mem_pos_calc_to_string(idx, rdh.data_format(), rdh_mem_pos);
-            generate_status_word_view(word, &mem_pos_str, &mut stdio_lock)?;
-        }
-    }
-    Ok(())
-}
-
-fn generate_status_word_view(
-    word: &[u8],
-    mem_pos_str: &str,
-    stdio_lock: &mut std::io::StdoutLock,
-) -> Result<(), Box<dyn std::error::Error>> {
-    match ItsPayloadWord::from_id(word[9]) {
-        Ok(word_type) => {
-            generate_its_readout_frame_word_view(word_type, word, mem_pos_str, stdio_lock)?
-        }
-        Err(e) => {
-            let word_str = format_word_slice(word);
-            let trimmed_mem_pos_str = mem_pos_str.trim();
-            log::error!(
-                "{trimmed_mem_pos_str} {e}: {:#02X} found in: {word_str}",
-                word[9]
+            let mem_pos_str = super::mem_pos_calc_to_string(
+                idx,
+                rdh.data_format(),
+                rdh_mem_pos,
+                disable_styled_view,
             );
-        }
-    }
-
-    Ok(())
-}
-
-/// Generates a human readable view of ITS readout frame words + data words based on the raw word, word type, and memory position.
-///
-/// Takes:
-///     * The word byte slice
-///     * The type of PayloadWord from the ITS payload protocol
-///     * The memory position of the word
-fn generate_its_readout_frame_word_view(
-    word_type: crate::analyze::validators::its::lib::ItsPayloadWord,
-    gbt_word_slice: &[u8],
-    mem_pos_str: &str,
-    stdio_lock: &mut std::io::StdoutLock,
-) -> Result<(), std::io::Error> {
-    use crate::words::its::status_words::util as sw_util;
-
-    let word_slice_str = crate::analyze::view::lib::format_word_slice(gbt_word_slice);
-    match word_type {
-        ItsPayloadWord::DataWord => writeln!(stdio_lock, "{mem_pos_str} DATA {word_slice_str}")?,
-        ItsPayloadWord::TDH => {
-            let trigger_str = sw_util::tdh_trigger_as_string(gbt_word_slice);
-            let continuation_str = sw_util::tdh_continuation_as_string(gbt_word_slice);
-            let no_data_str = sw_util::tdh_no_data_as_string(gbt_word_slice);
-            let trig_orbit_bc_str = sw_util::tdh_trigger_orbit_bc_as_string(gbt_word_slice);
-            writeln!(
-                            stdio_lock,
-                            "{mem_pos_str} TDH {word_slice_str} {trigger_str}  {continuation_str}        {no_data_str} {trig_orbit_bc_str:>42}"
-                        )?;
-        }
-
-        ItsPayloadWord::TDT => {
-            let packet_status_str = sw_util::tdt_packet_done_as_string(gbt_word_slice);
-            let error_reporting_str = sw_util::ddw0_tdt_lane_status_as_string(gbt_word_slice);
-            writeln!(
-                            stdio_lock,
-                            "{mem_pos_str} TDT {word_slice_str} {packet_status_str:>18}                             {error_reporting_str}",
-                        )?;
-        }
-        ItsPayloadWord::IHW => {
-            writeln!(stdio_lock, "{mem_pos_str} IHW {word_slice_str}")?;
-        }
-
-        ItsPayloadWord::DDW0 => {
-            let error_reporting_str = sw_util::ddw0_tdt_lane_status_as_string(gbt_word_slice);
-
-            writeln!(
-                            stdio_lock,
-                            "{mem_pos_str} DDW {word_slice_str}                                                {error_reporting_str}",
-                        )?;
-        }
-        ItsPayloadWord::CDW => {
-            writeln!(
-                            stdio_lock,
-                            "{mem_pos_str} CDW {word_slice_str}                                                ",
-                        )?;
-        }
-        ItsPayloadWord::IHW_continuation
-        | ItsPayloadWord::TDH_continuation
-        | ItsPayloadWord::TDH_after_packet_done => {
-            unsafe {
-                // This function receives only simple types,
-                //  as they are coming from ItsPayloadWord::from_id() and not from the FSM that can determine more complex types
-                std::hint::unreachable_unchecked()
-            }
+            super::generate_status_word_view(
+                word,
+                &mem_pos_str,
+                &mut stdio_lock,
+                disable_styled_view,
+                true,
+            )?;
         }
     }
     Ok(())
