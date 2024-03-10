ddf24bad7153f93ea6322d4f15beacbe3e51f0ec
Marc Konig <mbkj@tutamail.com>
2024-03-10 20:46:16 +0100

    benchmark collecting trigger stats

--- fastpasta/benches/benchmarks.rs
+++ fastpasta/benches/benchmarks.rs
@@ -4,6 +4,7 @@ mod io_bench;
 mod rdh_deserialize_bench;
 mod rdh_serialize_bench;
 mod rdh_validation_bench;
+mod trigger_stats;
 
 criterion_group!(
     benches,
@@ -11,6 +12,7 @@ criterion_group!(
     io_bench::bench_buffer_capacity,
     rdh_deserialize_bench::bench_deserialization,
     rdh_serialize_bench::bench_serialization_write,
-    rdh_validation_bench::bench_rdh_sanity_check
+    rdh_validation_bench::bench_rdh_sanity_check,
+    trigger_stats::bench_collect_trigger_stats,
 );
 criterion_main!(benches);
